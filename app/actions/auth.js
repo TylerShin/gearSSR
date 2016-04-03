5e15cb4927cf46cbd321ea4d0840b47b059d47d2
Tylor Shin <shincode@icloud.com>
2016-04-04 08:20:17 +0900

    Made local storage save method after sign in

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -4,6 +4,14 @@ import { CALL_API } from 'middleware/api';
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_UP = 'SIGN_UP';
 
+export function saveLocalStorage(auth) {
+  if (auth.access_token) {
+    localStorage.clear();
+    const userInJson = JSON.stringify(auth);
+    localStorage.setItem('currentUser', userInJson);
+  }
+}
+
 export function signIn(email, password) {
   return {
     [CALL_API]: {
--- app/reducers/auth.js
+++ app/reducers/auth.js
@@ -3,9 +3,11 @@ import * as ActionType from 'actions/auth';
 function authReducer(state = {}, action) {
   switch (action.type) {
     case ActionType.SIGN_IN:
+      ActionType.saveLocalStorage(action.response);
       return {
         currentUser: action.response
       };
+
     default:
       return state;
   }
